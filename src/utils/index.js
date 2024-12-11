b940285ace9d26cc1f9f1df137ddc690e9f27241
ALTHAF HUSSAIN <althafhussainpc22@gmail.com>
2024-12-11 23:22:04 +0530

    components removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,22 @@
-import _ from 'lodash';
+import _ from "lodash";
 
 import jsPDF from "jspdf";
 
 export const generateProductCatalogPDF = () => {
-
   const doc = new jsPDF({
     format: "A4",
-    unit: "pt"
+    unit: "pt",
   });
 
   doc.setFont("Inter-Regular", "normal");
 
-  let targetReport = document.getElementById('downloadReport-uniqueID')
+  let targetReport = document.getElementById("downloadReport-uniqueID");
 
   doc.html(targetReport, {
     async callback(doc) {
       await doc.save("productList");
     },
     width: 600,
-    windowWidth: 600
+    windowWidth: 600,
   });
-
 };
