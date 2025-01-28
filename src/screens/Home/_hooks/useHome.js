6671cf125cffb58e7df66c1070797d06d1679992
Mohan C <86504787+mohanc046@users.noreply.github.com>
2025-01-28 13:40:43 +0530

    Updated changes - initial commit

--- src/screens/Home/_hooks/useHome.js
+++ src/screens/Home/_hooks/useHome.js
@@ -276,7 +276,7 @@ export const useHome = () => {
 
   const visitStoreData = [
     {
-      shopLink: `http://${getStoreInfo()?.store?.businessName}.afras.in`,
+      shopLink: `http://${getStoreInfo()?.store?.domainName}.${process.env.REACT_APP_DOMAIN_EXTENSION}`,
       status: "LIVE",
       action: "Visit"
     }
